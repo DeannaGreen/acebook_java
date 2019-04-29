7d4051aabea32db3208ca2929cb98eb9ccd4e523
Deanna Green <deanna@16vmini.co.uk>
2019-04-29 12:02:26 +0100

    can put posts over multiple new lines;

--- src/main/js/posts/post.js
+++ src/main/js/posts/post.js
@@ -4,7 +4,9 @@ const Post = (props) => {
 	return (
 		<div className='post-main'>
 			<div className='post-content'>
-				{props.post.content}
+				{props.post.content.split("\n").map((i,key) => {
+                    return <div key={key}>{i}</div>;
+                })}
 			</div>
 			<div className='post-title'>
                 {props.post.title}
